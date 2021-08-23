0932e47065219329e2d2583b961211549e4f0a7a
joaojsrbr <joaovitor.jsr@gmail.com>
2021-08-23 19:47:00 -0300

    titei o cache

--- src/main/java/joao/main/jsr/JsrApplication.java
+++ src/main/java/joao/main/jsr/JsrApplication.java
@@ -2,14 +2,12 @@ package joao.main.jsr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
-@EnableCaching
 public class JsrApplication {
 
 	public static void main(String[] args) {
