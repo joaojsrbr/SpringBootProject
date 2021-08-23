f3ab9bc489a82041298d1c8b79c1de4c60317af1
joaojsrbr <joaovitor.jsr@gmail.com>
2021-08-23 19:50:38 -0300

    new

--- src/main/java/joao/main/jsr/JsrApplication.java
+++ src/main/java/joao/main/jsr/JsrApplication.java
@@ -2,7 +2,7 @@ package joao.main.jsr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
 
 
 
